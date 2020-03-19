b9928783e42809f0cc6ed5636cd53740f5d92126
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-19 00:01:54 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,8 +1,5 @@
 def meal_choice("broccoli", "macaroni", protein = "tofu")
-  if meal_choice
-    puts "What a nutritious meal!"
-  else
-    puts "A plate of #{protein} with #{veg1} and #{veg2}."
-  end
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 meal_choice
